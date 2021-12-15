065ce92485a5c6e4c2e0eaf33034011d3601ddf3
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2021-12-15 19:52:59 -0300

    Day 15 puzzles

---
+++ day15-1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include "string"
+#include <map>
+
+const int WIDTH = 100;
+const int HEIGHT = 100;
+
+int map[WIDTH][HEIGHT];
+int shortest_path[WIDTH][HEIGHT] = {0};
+
+void calc_risk(int i, int j, int prev_total) {
+    //std::cout << i << ',' << j << '\n';
+
+    if (i < 0 || j < 0 || i == WIDTH || j == HEIGHT) {
+        return;
+    }
+
+    int cur_total = prev_total + map[i][j];
+
+    //std::cout << cur_total << ',' << shortest_path[i][j] << '\n';
+
+    if (shortest_path[i][j] == 0 || cur_total < shortest_path[i][j]) {
+        shortest_path[i][j] = cur_total;
+
+        calc_risk(i + 1, j, cur_total);
+        calc_risk(i, j + 1, cur_total);
+        calc_risk(i - 1, j, cur_total);
+        calc_risk(i, j - 1, cur_total);
+    }
+}
+
+int main() {
+    std::fstream fin("input.txt", std::fstream::in);
+    std::string line;
+
+    int j = 0;
+    while (std::getline(fin, line)) {
+        int i = 0;
+
+        for (auto risk: line) {
+            map[i][j] = risk - '0';
+
+            i++;
+        }
+
+        j++;
+    }
+
+    calc_risk(WIDTH - 1, HEIGHT - 1, 0);
+
+    std::cout << shortest_path[0][0] - map[0][0];
+
+    return 0;
+}
---
+++ day15-2/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <queue>
+
+const int WIDTH = 500;
+const int HEIGHT = 500;
+
+int map[WIDTH][HEIGHT];
+int shortest_path[WIDTH][HEIGHT] = {0};
+
+void calc_risk(int start_i, int start_j) {
+    //std::cout << i << ',' << j << '\n';
+
+    std::deque<std::pair<int, int>> point_queue;
+
+    point_queue.emplace_back(start_i, start_j);
+    shortest_path[start_i][start_j] = 0;
+//    int min_i = WIDTH;
+//    int min_j = HEIGHT;
+
+    while (!point_queue.empty()) {
+        auto cur_point = point_queue.front();
+        point_queue.pop_front();
+
+        auto[i, j] = cur_point;
+
+
+        if (i + 1 <= (WIDTH - 1) &&
+            (shortest_path[i + 1][j] == 0 || shortest_path[i][j] + map[i + 1][j] < shortest_path[i + 1][j])) {
+            shortest_path[i + 1][j] = shortest_path[i][j] + map[i + 1][j];
+            point_queue.emplace_back(i + 1, j);
+        }
+
+        if (j + 1 <= (HEIGHT - 1) &&
+            (shortest_path[i][j + 1] == 0 || shortest_path[i][j] + map[i][j + 1] < shortest_path[i][j + 1])) {
+            shortest_path[i][j + 1] = shortest_path[i][j] + map[i][j + 1];
+            point_queue.emplace_back(i, j + 1);
+        }
+
+        if (i - 1 >= 0 &&
+            (shortest_path[i - 1][j] == 0 || shortest_path[i][j] + map[i - 1][j] < shortest_path[i - 1][j])) {
+            shortest_path[i - 1][j] = shortest_path[i][j] + map[i - 1][j];
+            point_queue.emplace_back(i - 1, j);
+        }
+
+        if (j - 1 >= 0 &&
+            (shortest_path[i][j - 1] == 0 || shortest_path[i][j] + map[i][j - 1] < shortest_path[i][j - 1])) {
+            shortest_path[i][j - 1] = shortest_path[i][j] + map[i][j - 1];
+            point_queue.emplace_back(i, j - 1);
+        }
+
+
+        //std::cout << cur_total << ',' << shortest_path[i][j] << '\n';
+    }
+
+    //std::cout << cur_total << ',' << shortest_path[i][j] << '\n';
+}
+
+int main() {
+    std::fstream fin("input.txt", std::fstream::in);
+    std::string line;
+
+    int j = 0;
+    while (std::getline(fin, line)) {
+        int i = 0;
+
+        for (auto risk: line) {
+            int risk_val = risk - '0';
+
+            for (int m = 0; m < 5; m++) {
+                for (int n = 0; n < 5; n++) {
+                    int cur_risk = risk_val + m + n;
+
+                    if (cur_risk > 9) {
+                        cur_risk -= 9;
+                    }
+
+                    map[m * 100 + i][n * 100 + j] = cur_risk;
+                }
+            }
+
+            i++;
+        }
+
+        j++;
+    }
+
+//    for (int i = 0; i < WIDTH; i++) {
+//        for (int j = 0; j < HEIGHT; j++) {
+//            std::cout << (int) map[i][j];
+//        }
+//        std::cout << '\n';
+//    }
+
+    calc_risk(0, 0);
+
+    std::cout << shortest_path[WIDTH - 1][HEIGHT - 1];
+
+    return 0;
+}
